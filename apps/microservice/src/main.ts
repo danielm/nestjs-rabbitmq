f3e312b98b915f825555a0112f2991bc10c14f06
Alvaro Daniel Morales <daniel85mg@gmail.com>
2024-04-15 15:06:15 -0300

    rabbit

--- apps/microservice/src/main.ts
+++ apps/microservice/src/main.ts
@@ -7,16 +7,25 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://followthe:rabbit@localhost:5672'],
+        queue: 'cats_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    }
   );
+  await app.listen();
+
+  Logger.log(`🚀 RabbitMQ microservice is running!`);
 }
 
 bootstrap();
